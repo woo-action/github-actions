7ccbfec53e4100ea9479f16b6083ab00aa9e5186
우태건[IAM Service] <taegeon.woo@navercorp.com>
2024-06-07 11:53:04 +0900

    editApp.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd
+          Learn GithubAction cicd edited
         </a>
       </header>
     </div>
